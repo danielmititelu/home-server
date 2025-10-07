beaf21049f71ca16bb81a860ba693a1554a76a90
Daniel Mititelu <mttl.daniel@gmail.com>
2025-10-07 11:26:55 +0300

    add samba

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 cp /home-server/config/srv/caddy/Caddyfile /srv/caddy/Caddyfile
 cp /home-server/config/srv/glance/config/glance.yml /srv/glance/config/glance.yml
 cp /home-server/.env /srv/.env
+cp /home-server/config/etc/samba/smb.conf /etc/samba/smb.conf
