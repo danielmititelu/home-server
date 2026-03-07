220c43b2169065b1e348ff8c70c5b2ca5584a271
Daniel Mititelu <mttl.daniel@gmail.com>
2026-03-07 11:55:11 +0200

    update sync script

---
+++ scripts/sync.sh
@@ -0,0 +1,4 @@
+cp /home-server/config/srv/caddy/Caddyfile /srv/caddy/Caddyfile
+ln -sf /home-server/config/etc/samba/smb.conf /etc/samba/smb.conf
+ln -sf /home-server/config/etc/systemd/system/home-bot.service /etc/systemd/system/home-bot.service
+ln -sf /home-server/config/etc/systemd/system/kodi.service /etc/systemd/system/kodi.service
