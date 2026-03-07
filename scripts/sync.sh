22a72cd89763e5605536090dbe9e61b918d06547
Daniel Mititelu <mttl.daniel@gmail.com>
2026-03-07 12:13:44 +0200

    add metis command

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,7 @@
-[ "$EUID" -ne 0 ] && exec sudo "$0" "$@"
+#!/bin/sh
+[ "$(id -u)" -ne 0 ] && exec sudo "$0" "$@"
 
 ln -sf /home-server/config/etc/samba/smb.conf /etc/samba/smb.conf
 ln -sf /home-server/config/etc/systemd/system/home-bot.service /etc/systemd/system/home-bot.service
 ln -sf /home-server/config/etc/systemd/system/kodi.service /etc/systemd/system/kodi.service
+ln -sf /home-server/scripts/metis /usr/local/bin/metis
