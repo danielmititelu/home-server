9eda58717ec033da7b9508000b7cf1819f981a38
Daniel Mititelu <mttl_daniel@yahoo.com>
2026-03-08 19:52:06 +0200

    update sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,4 +5,10 @@ ln -sf /home-server/config/etc/samba/smb.conf /etc/samba/smb.conf
 ln -sf /home-server/config/etc/systemd/system/homebot.service /etc/systemd/system/homebot.service
 ln -sf /home-server/config/etc/systemd/system/kodi.service /etc/systemd/system/kodi.service
 ln -sf /home-server/scripts/metis /usr/local/bin/metis
-ln -sf /home-server/config/etc/cron.d/home-server /etc/cron.d/home-server
+rm -f /etc/cron.d/home-server
+# copy the cron file instead of symlinking it because cron needs root
+# ownership to read the file but in the repo it needs user ownership 
+# in order to be editable without sudo
+cp /home-server/config/etc/cron.d/home-server /etc/cron.d/home-server
+chown root:root /etc/cron.d/home-server
+chmod 644 /etc/cron.d/home-server
